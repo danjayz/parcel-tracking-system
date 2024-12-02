2076ecda34e91591834569825f3d9fe5647a239f
Dananjaya <hello.danjay@gmail.com>
2024-12-03 00:11:10 +0530

    make database integrations

---
+++ backend/database/migrations/2024_12_02_183647_create_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->string('name', length: 100);
+            $table->string('email', length: 200)->unique();
+            $table->string('password', length: 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
